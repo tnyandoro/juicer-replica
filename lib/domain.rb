3bc0aad7a3f2024fe7569efe8baec4751cf7fdb6
Tendai <tnyandoro@gmail.com>
2026-02-16 20:00:01 +0200

    Add domain module entry point

---
+++ lib/domain.rb
@@ -0,0 +1,14 @@
+require_relative 'domain/value_objects/fruit_size'
+require_relative 'domain/value_objects/ripeness_level'
+require_relative 'domain/value_objects/juice_volume'
+require_relative 'domain/entities/fruit'
+require_relative 'domain/entities/juice_tank'
+require_relative 'domain/entities/waste_bin'
+require_relative 'domain/entities/press_unit'
+require_relative 'domain/entities/filter_unit'
+require_relative 'domain/juicer_machine'
+
+module CommercialJuicer
+  module Domain
+  end
+end
